0392b4c1b24d1ee2e555982d04822e98576976cb
Sravankumar <kumarsravan098@gmail.com>
2025-03-21 22:54:50 +0530

    imported files

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,2 +1,3 @@
 const math=require('./math');
 console.log(math.sum(2,3));
+console.log(math.student.sravan.marks);
--- BACKEND/math.js
+++ BACKEND/math.js
@@ -1,3 +1,21 @@
-exports.sum=function(a,b){
-    return a+b;
-}
+const student = {
+    sravan: {
+        marks: 93,
+        class: "cse-c",
+        getgrade() {
+            console.log("a grade");
+        }
+    },
+    dhanush: {
+        marks: 92,
+        class: "cse-b"
+    }
+};
+
+// Export both `sum` and `student` in a single object
+module.exports = {
+    sum: function(a, b) {
+        return a + b;
+    },
+    student: student
+};
